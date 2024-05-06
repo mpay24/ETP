80249d539c502dfce3d286ccf22cbc7365134a48
Milko Daskalov <milko.daskalov@unzer.com>
2024-05-06 12:14:44 +0200

    added target build flags

--- Package.swift
+++ Package.swift
@@ -15,7 +15,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ETP"),
+            name: "ETP",
+            publicHeadersPath: "./",
+            cSettings: [.unsafeFlags(["-I/usr/include/libxml2", "-fno-objc-arc"])],
+            linkerSettings: [.unsafeFlags(["-lxml2"])]
+        ),
         .testTarget(
             name: "ETPTests",
             dependencies: ["ETP"]),
