593260174fc181bf12a2720ab548d3e4b68224e0
Milko Daskalov <mdaskalov@outlook.com>
2024-12-10 13:25:11 +0100

    added simple test case (without auth data)

--- Tests/ETPTests/ETPTests.swift
+++ Tests/ETPTests/ETPTests.swift
@@ -1,12 +1,34 @@
-import XCTest
+import Testing
+import Foundation
 @testable import ETP
 
-final class ETPTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+let authUser = "u9xxxx"
+let authPassword = "xxxxx"
+let merchantID:UInt32 = 90000
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+final class ETPTests {
+    @Test func testAcceptPayment() async throws {
+        var service = ETP("https://test.mpay24.com")
+        service.authentication = .basic(username: authUser, password: authPassword)
+        service.soapRequest = { print("Request:\n\($0)") }
+        service.soapResponse = { print("Response:\n\($0)") }
+        let payment = ETP.PaymentCC (
+            amount: 100,
+            currency: "EUR",
+            brand: "VISA",
+            identifier: "4444333322221111",
+            expiry: 2906
+        )
+        let request = ETP.AcceptPayment(
+            merchantID: merchantID,
+            tid: "testing",
+            pType: .CC,
+            payment: payment
+        )
+        let response = try await service.AcceptPayment(request)
+        
+        #expect(response.status == .OK)
+        #expect(response.returnCode == "OK")
+        #expect(response.mpayTID != nil)
     }
 }
